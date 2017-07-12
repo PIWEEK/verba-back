e6e07aaf2c03c118ea9af59bcd84f3323469c93e
Tere <teresa.delatorre@kaleidos.net>
2017-07-12 12:43:07 +0200

    Related quotes

--- quotes/serializers.py
+++ quotes/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from quotes.models import Author, Quote, Tag
+from verba import settings
 
 
 class TagSerializer(serializers.HyperlinkedModelSerializer):
@@ -20,7 +21,15 @@ class AuthorSerializer(serializers.HyperlinkedModelSerializer):
 class QuoteSerializer(serializers.HyperlinkedModelSerializer):
     author = serializers.HyperlinkedRelatedField(view_name='author-detail', read_only=True)
     tags = serializers.HyperlinkedRelatedField(many=True, view_name='tag-detail', read_only=True)
+    related_quotes = serializers.SerializerMethodField()
 
     class Meta:
         model = Quote
-        fields = ('id', 'text', 'author', 'date', 'source', 'tags')
+        fields = ('id', 'url', 'text', 'author', 'date', 'source', 'tags', 'related_quotes')
+
+    def get_related_quotes(self, obj):
+        related_quotes = []
+        for quote in Quote.objects.all().order_by('?').exclude(id=obj.id)[:3]:
+            related_quotes.append({'text': quote.text, 'author_name': quote.author.name, 'url': '{}/api/quotes/{}'.format(settings.BASE_URL, quote.id)})
+
+        return related_quotes
--- verba/settings.py
+++ verba/settings.py
@@ -133,3 +133,6 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 2
 }
+
+
+BASE_URL = os.environ.get('DOMAIN_URL', 'http://localhost:8000')
